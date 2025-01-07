68a48fc03595f2553b642a236761d0db49ffa80a
Natpnk <patrick@natpnk.nl>
2025-01-08 00:08:40 +0100

    Updated namespace & added class documentation

--- src/TransIPServiceProvider.php
+++ src/TransIPServiceProvider.php
@@ -1,17 +1,25 @@
 <?php
 
-namespace IntVent\TransIPLaravel;
+namespace Natpnk\TransIPLaravel;
 
-use Illuminate\Support\ServiceProvider;
 use Transip\Api\Library\TransipAPI;
+use Illuminate\Support\ServiceProvider;
 
-class TransIPServiceProvider extends ServiceProvider
-{
+/**
+ * TransIP Service Provider
+ *
+ * This service provider integrates TransIP API into a Laravel application.
+ * It sets up configuration, binds services, and provides access to the TransIP client.
+ */
+class TransIPServiceProvider extends ServiceProvider {
+    
     /**
-     * Perform post-registration booting of services.
+     * Perform post-registration
+     *
+     * This method publishes the configuration file     
      */
-    public function boot()
-    {
+    public function boot(){
+        
         $this->publishes([
             __DIR__.'/../config/transip.php' => config_path('transip.php'),
         ], 'config');
@@ -19,18 +27,23 @@ class TransIPServiceProvider extends ServiceProvider
 
     /**
      * Register any package services.
+     *
+     * This method merges the configuration file and binds the TransIP client
+     * to the Laravel service container.
      */
-    public function register()
-    {
+    public function register(){
+        
         $this->mergeConfigFrom(__DIR__.'/../config/transip.php', 'transip');
 
         $this->app->bind(TransipAPI::class, function () {
+            
             $config = config('transip');
             $api = new TransipAPI(
                 $config['login'],
                 $config['privateKey'],
                 $config['generateWhitelistOnlyTokens']
             );
+            
             $api->setTestMode($config['testMode']);
 
             return $api;
@@ -42,10 +55,11 @@ class TransIPServiceProvider extends ServiceProvider
     /**
      * Get the services provided by the provider.
      *
-     * @return array
+     * This method returns the services that the provider registers.
+     *
+     * @return array An array of service names.
      */
-    public function provides()
-    {
+    public function provides(){
         return ['transip'];
     }
 }
