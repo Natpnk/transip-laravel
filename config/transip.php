645e42ea26cad2248faa4be2fbb4ab5cd87113e8
Natpnk <patrick@natpnk.nl>
2025-01-08 00:08:01 +0100

    Removed empty lines

--- config/transip.php
+++ config/transip.php
@@ -8,13 +8,9 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-
+    
     'login' => env('TRANSIP_LOGIN'),
-
     'privateKey' => env('TRANSIP_PRIVATE_KEY'),
-
     'generateWhitelistOnlyTokens' => env('TRANSIP_WHITELIST_ONLY', true),
-
     'testMode' => env('TRANSIP_TEST_MODE', false),
-
 ];
